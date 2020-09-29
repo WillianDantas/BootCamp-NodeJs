4724da70e498a7cc4ec7ec9f6e79946090b71a62
Willian Dantas <williansilva1991@gmail.com>
2020-09-29 00:30:01 -0300

    array

---
+++ conceitos/array/1-array.ts
@@ -0,0 +1,25 @@
+const divs = document.querySelectorAll('div'); // ArrayNodelist
+const arr = Array.from(divs); // Transforma em array
+
+arr.pop();
+
+arr.shift();
+
+arr.push();
+
+arr.pop() // remove ultimo elemento do array;
+
+
+arr.unshift(); // adiconar no inicio do array
+
+arr.shift() // remove no inicio
+
+arr.concat() // merge entre duas lista
+
+arr.slice();
+
+arr.splice();
+
+frutas = ["melacia", "banana"];
+
+frutas.splice(1, 0, "acerola");
